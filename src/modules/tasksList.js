eecdcfca8f8322d7c04fa2a0bc373bc86f02a23a
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-05 21:50:28 -0500

    Add index.html structure

---
+++ src/modules/tasksList.js
@@ -0,0 +1,22 @@
+export const tasksList = [
+{
+description: 'Take the dog out for a walk',
+completed: false,
+index: 1,
+},
+{
+  description: 'Create To-do app',
+  completed: false,
+  index: 2,
+},
+{
+  description: 'Solve world hunger with JavaScript',
+  completed: false,
+  index: 3,
+},
+{
+  description: 'Wash the dishes',
+  completed: false,
+  index: 4,
+},
+];
