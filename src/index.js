f0c4ddd24fb9f76cbaf1e789fc3872b3fb20a487
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-05 23:35:32 -0500

    Add JS modules and finish structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import _ from 'lodash';
-import './style.css'
+// import _ from 'lodash';
+import './style.css';
+import { createHTML } from './modules/createHTML.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener('DOMContentLoaded', createHTML);
--- src/modules/tasksList.js
+++ src/modules/tasksList.js
@@ -1,22 +1,22 @@
 export const tasksList = [
-{
-description: 'Take the dog out for a walk',
-completed: false,
-index: 1,
-},
-{
-  description: 'Create To-do app',
-  completed: false,
-  index: 2,
-},
-{
-  description: 'Solve world hunger with JavaScript',
-  completed: false,
-  index: 3,
-},
-{
-  description: 'Wash the dishes',
-  completed: false,
-  index: 4,
-},
+  {
+    description: 'Take the dog out for a walk',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Create To-do app',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Solve world hunger with JavaScript',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Wash the dishes',
+    completed: false,
+    index: 3,
+  },
 ];
