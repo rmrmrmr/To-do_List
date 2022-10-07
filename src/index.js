ba46a85b9b0e220ef014c0d7e4f7d99d4cb857ac
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-06 22:33:01 -0500

    Add functionality to add, remove & edit tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
-// import _ from 'lodash';
 import './style.css';
-import { createHTML } from './modules/createHTML.js';
+import Methods from './modules/storageMethods.js';
 
-window.addEventListener('DOMContentLoaded', createHTML);
+Methods.setIndex();
+Methods.render();
+
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const list = document.querySelector('#list');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const description = input.value;
+  Methods.add(description);
+  Methods.render();
+  form.reset();
+});
+
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('vertical-menu')) {
+    e.target.classList.remove('show');
+    e.target.classList.add('hide');
+    e.target.nextElementSibling.classList.remove('hide');
+    e.target.nextElementSibling.classList.add('show');
+    e.target.previousElementSibling.removeAttribute('disabled');
+    e.target.previousElementSibling.focus();
+  }
+
+  if (e.target.textContent === 'Delete') {
+    e.target.parentElement.parentElement.remove();
+    Methods.remove(e.target.parentElement.parentElement.id - 1);
+  }
+
+  if (e.target.textContent === 'Save') {
+    e.target.parentElement.classList.add('hide');
+    e.target.parentElement.classList.remove('show');
+    e.target.parentElement.previousElementSibling.classList.add('show');
+    e.target.parentElement.previousElementSibling.classList.remove('hide');
+    e.target.parentElement.previousElementSibling.previousElementSibling.setAttribute('disabled', 'disabled');
+    Methods.edit(e.target.parentElement.previousElementSibling.previousElementSibling.value,
+      e.target.parentElement.parentElement.id - 1);
+  }
+});
