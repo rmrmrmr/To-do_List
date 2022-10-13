6ce3bcd3b6bd4e9793377d453628a948d752e4ec
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-13 16:49:53 -0500

    Move creation of array to DOMCONTENTLOADED

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './style.css';
 import Methods from './modules/functions.js';
 import { listSection, addTaskBttn, clearBttn } from './modules/vars.js';
 
-let tasksArr = new Methods();
+// let tasksArr = new Methods();
 
-addTaskBttn.addEventListener('submit', (evt) => {
+addTaskBttn.addEventListener('submit', (evt, tasksArr) => {
   evt.preventDefault();
   listSection.innerHTML = '';
 
@@ -14,12 +14,13 @@ addTaskBttn.addEventListener('submit', (evt) => {
   Methods.addToLocalStorage(tasksArr);
 });
 
-clearBttn.addEventListener('click', () => {
+clearBttn.addEventListener('click', (tasksArr) => {
   Methods.clearTasks(tasksArr);
   tasksArr = Methods.clearTasks(tasksArr);
 });
 
 window.addEventListener('DOMContentLoaded', () => {
+  let tasksArr = new Methods();
   Methods.parseLS(tasksArr);
   tasksArr = Methods.parseLS(tasksArr);
   Methods.createHTML(tasksArr);
