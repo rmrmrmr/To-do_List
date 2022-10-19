be162d4e29209a4d017cec47ee653673c55dfe5e
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-19 14:23:06 -0500

    Setup jest

---
+++ src/addAndDelete.test.js
@@ -0,0 +1,17 @@
+// import './style.css';
+// import Methods from './modules/functions';
+// import {
+// listSection, addTaskBttn, clearBttn, renewBttn
+//  } from './modules/vars';
+
+it('works', () => {
+  expect(1).toBe(1);
+});
+
+// ADD TASKS
+// getTaskName
+// setIndex
+// createHTML
+// addToLocalStorage
+
+// deleteTask
